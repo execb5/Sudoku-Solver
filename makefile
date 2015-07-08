81c925c08d9744ba131b3024581a8565660c3788
Matthias Nunes <matthiasnunes@gmail.com>
2015-07-08 14:28:23 -0300

    Reads perfectly the file
    Also adds a make run rule

--- makefile
+++ makefile
@@ -17,6 +17,9 @@ $(ODIR)/%.o: $(SDIR)/%.c $(DEPS)
 main: $(OBJ)
 	$(CC) -o $@ $^ $(CFLAGS)
 
+run: main
+	./$^
+
 testLeak: main
 	valgrind --leak-check=full -v ./$^
 
--- src/main.c
+++ src/main.c
@@ -1,22 +1,37 @@
 #include <stdio.h>
+#include <string.h>
+
+#define LINE_SIZE 128
 
 int
 main(int argc, const char* argv[])
 {
         static const char filename[] = "schema";
-        FILE *file = fopen ( filename, "r" );
-        if ( file != NULL )
+        FILE* file = fopen ( filename, "r" );
+        if (file != NULL)
         {
-                char line [ 128 ]; /* or other suitable maximum line size */
-                while ( fgets ( line, sizeof line, file ) != NULL ) /* read a line */
+                char line[LINE_SIZE];
+                while (fgets(line, sizeof line, file) != NULL) //read a line
                 {
-                        fputs ( line, stdout ); /* write the line */
+                        size_t ln = strlen(line) - 1;
+                        if (line[ln] == '\n')
+                        {
+                                line[ln] = '\0'; //removes the trailing newline
+                        }
+                        
+                        char* ch = strtok(line, "\ ");
+                        while (ch != NULL)
+                        {
+                                printf("%s ", ch);
+                                ch = strtok ( NULL, "\ " );
+                        }
+                        printf ("\n");
                 }
-                fclose ( file );
+                fclose(file);
         }
         else
         {
-                perror ( filename ); /* why didn't the file open? */
+                perror(filename); //why didn't the file open?
         }
         return 0;
 }
