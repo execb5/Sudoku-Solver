94760e778775a9a006fb3c878815815cc0631bde
Matthias Nunes <matthiasnunes@gmail.com>
2015-07-10 20:51:47 -0300

    Finishes refactoring

--- include/Sudoku.h
+++ include/Sudoku.h
@@ -1,7 +1,5 @@
 #define SUDOKU_SIZE 9
 #define LINE_SIZE 128
-#define FIRST_SECTION 3
-#define SECOND_SECTION 6
 
 typedef enum {false, true} bool;
 
@@ -9,27 +7,9 @@ typedef struct {
         int board[SUDOKU_SIZE][SUDOKU_SIZE];
 } Sudoku;
 
-extern int checker[SUDOKU_SIZE];
-
-bool checkChecker(void);
 Sudoku* copyBoard(Sudoku* su);
-bool freePos(Sudoku* su, int y, int x);
-bool is1stQOk(Sudoku* su);
-bool is2ndQOk(Sudoku* su);
-bool is3rdQOk(Sudoku* su);
-bool is4thQOk(Sudoku* su);
-bool is5thQOk(Sudoku* su);
-bool is6thQOk(Sudoku* su);
-bool is7thQOk(Sudoku* su);
-bool is8thQOk(Sudoku* su);
-bool is9thQOk(Sudoku* su);
-bool isBoardCorrect(Sudoku* su);
-bool isColumnCorrect(Sudoku* su, int column);
-bool isLineCorrect(Sudoku* su, int line);
 bool isValid(Sudoku* su, int val, int row, int column);
 void printBoard(Sudoku* su);
 Sudoku* readSudokuFromFile(const char* fileName);
-void resetChecker(void);
 bool solve(Sudoku* su);
 bool solveAux(Sudoku* su, int row, int column);
-bool write(Sudoku* su, int val, int row, int column);
--- src/main.c
+++ src/main.c
@@ -10,26 +10,6 @@ main(int argc, const char* argv[])
 
         Sudoku* read = readSudokuFromFile(filename);
 
-        Sudoku test = { { { 0, 5, 0, 0, 6, 0, 0, 0, 1 }
-                        , { 0, 0, 4, 8, 0, 0, 0, 7, 0 }
-                        , { 8, 0, 0, 0, 0, 0, 0, 5, 2 }
-                        , { 2, 0, 0, 0, 5, 7, 0, 3, 0 }
-                        , { 0, 0, 0, 0, 0, 0, 0, 0, 0 }
-                        , { 0, 3, 0, 6, 9, 0, 0, 0, 5 }
-                        , { 7, 9, 0, 0, 0, 0, 0, 0, 8 }
-                        , { 0, 1, 0, 0, 0, 6, 5, 0, 0 }
-                        , { 5, 0, 0, 0, 3, 0, 0, 6, 0 } } };
-
-        Sudoku correct = { { { 5, 3, 4, 6, 7, 8, 9, 1, 2 }
-                           , { 6, 7, 2, 1, 9, 5, 3, 4, 8 }
-                           , { 1, 9, 8, 3, 4, 2, 5, 6, 7 }
-                           , { 8, 5, 9, 7, 6, 1, 4, 2, 3 }
-                           , { 4, 2, 6, 8, 5, 3, 7, 9, 1 }
-                           , { 7, 1, 3, 9, 2, 4, 8, 5, 6 }
-                           , { 9, 6, 1, 5, 3, 7, 2, 8, 4 }
-                           , { 2, 8, 7, 4, 1, 9, 6, 3, 5 }
-                           , { 3, 4, 5, 2, 8, 6, 1, 7, 9 } } };
-
         Sudoku hard = { { { 0, 0, 0, 0, 0, 0, 0, 0, 0 }
                         , { 0, 0, 0, 0, 0, 3, 0, 8, 5 }
                         , { 0, 0, 1, 0, 2, 0, 0, 0, 0 }
@@ -40,59 +20,16 @@ main(int argc, const char* argv[])
                         , { 0, 0, 2, 0, 1, 0, 0, 0, 0 }
                         , { 0, 0, 0, 0, 4, 0, 0, 0, 9 } } };
 
-        Sudoku* copy = copyBoard(&test);
-
-        printf("Read\n");
+        printf("Normal puzzle:\n");
         printBoard(read);
-        printf("\n------------------------------------\n");
-        printf("test\n");
-        printBoard(&test);
-        printf("\n------------------------------------\n");
-        printf("copy\n");
-        printBoard(copy);
-        printf("\n------------------------------------\n");
-        printf("correct\n");
-        printBoard(&correct);
-
-        printf("correct 1stQOk? %d\n", is1stQOk(&correct));
-        printf("test 1stQOk? %d\n", is1stQOk(&test));
-        printf("correct 2ndQOk? %d\n", is2ndQOk(&correct));
-        printf("test 2ndQOk? %d\n", is2ndQOk(&test));
-        printf("correct 3rdQOk? %d\n", is3rdQOk(&correct));
-        printf("test 3rdQOk? %d\n", is3rdQOk(&test));
-        printf("correct 4thQOk? %d\n", is4thQOk(&correct));
-        printf("test 4thQOk? %d\n", is4thQOk(&test));
-        printf("correct 5thQOk? %d\n", is5thQOk(&correct));
-        printf("test 5thQOk? %d\n", is5thQOk(&test));
-        printf("correct 6thQOk? %d\n", is6thQOk(&correct));
-        printf("test 6thQOk? %d\n", is6thQOk(&test));
-        printf("correct 7thQOk? %d\n", is7thQOk(&correct));
-        printf("test 7thQOk? %d\n", is7thQOk(&test));
-        printf("correct 8thQOk? %d\n", is8thQOk(&correct));
-        printf("test 8thQOk? %d\n", is8thQOk(&test));
-        printf("correct 9thQOk? %d\n", is9thQOk(&correct));
-        printf("test 9thQOk? %d\n", is9thQOk(&test));
-
-        printf("9th column correct correct? %d\n", isColumnCorrect(&correct, 8));
-        printf("9th column test correct? %d\n", isColumnCorrect(&test, 8));
-
-        printf("9th line correct correct? %d\n", isLineCorrect(&correct, 8));
-        printf("9th line test correct? %d\n", isLineCorrect(&test, 8));
-
-        printf("Is correct board correct? %d\n", isBoardCorrect(&correct));
-        printf("Is test board correct? %d\n", isBoardCorrect(&test));
-
         solve(read);
         printBoard(read);
-        solve(copy);
-        printBoard(copy);
         printf("Hard puzzle:\n");
         printBoard(&hard);
         solve(&hard);
         printBoard(&hard);
 
         free(read);
-        free(copy);
 
         return 0;
 }
