c433a8236ed7933c9f9cfbe480826ebc68718761
Matthias Nunes <matthiasnunes@gmail.com>
2015-07-10 21:16:38 -0300

    Adds hard puzzle to examples and cleans main.c

--- src/main.c
+++ src/main.c
@@ -9,24 +9,16 @@ main(int argc, const char* argv[])
 
         Sudoku* read = readSudokuFromFile(filename);
 
-        Sudoku hard = { { { 0, 0, 0, 0, 0, 0, 0, 0, 0 }
-                        , { 0, 0, 0, 0, 0, 3, 0, 8, 5 }
-                        , { 0, 0, 1, 0, 2, 0, 0, 0, 0 }
-                        , { 0, 0, 0, 5, 0, 7, 0, 0, 0 }
-                        , { 0, 0, 4, 0, 0, 0, 1, 0, 0 }
-                        , { 0, 9, 0, 0, 0, 0, 0, 0, 0 }
-                        , { 5, 0, 0, 0, 0, 0, 0, 7, 3 }
-                        , { 0, 0, 2, 0, 1, 0, 0, 0, 0 }
-                        , { 0, 0, 0, 0, 4, 0, 0, 0, 9 } } };
+        Sudoku* hard = readSudokuFromFile("examples/hard");
 
         printf("Normal puzzle:\n");
         printBoard(read);
         solve(read);
         printBoard(read);
         printf("Hard puzzle:\n");
-        printBoard(&hard);
-        solve(&hard);
-        printBoard(&hard);
+        printBoard(hard);
+        solve(hard);
+        printBoard(hard);
 
         free(read);
 
