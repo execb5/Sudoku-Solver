4408300ce52730b5cd58f75d840fef3d5e7d21ec
Matthias Nunes <matthiasnunes@gmail.com>
2015-07-14 05:36:50 -0300

    GUI prototype design is ready

--- makefile
+++ makefile
@@ -3,7 +3,7 @@ SDIR = ./src
 ODIR = ./obj
 
 CC = gcc
-CFLAGS = -Wall -g -I$(IDIR) `pkg-config --libs --cflags gtk+-2.0`
+CFLAGS = -Wall -g -I$(IDIR) `pkg-config --libs --cflags gtk+-3.0`
 
 _DEPS = sudoku.h
 DEPS = $(patsubst %,$(IDIR)/%,$(_DEPS))
--- src/sudokuGUI.c
+++ src/sudokuGUI.c
@@ -4,19 +4,6 @@
 #include <sudoku.h>
 #include <gtk/gtk.h>
 
-void
-print_msg(GtkWidget *widget, gpointer window)
-{
-
-        printf("Button clicked\n");
-}
-
-void
-load_msg(GtkWidget *widget, gpointer window)
-{
-        printf("Load selected\n");
-}
-
 GdkPixbuf*
 create_pixbuf(const gchar * filename)
 {
@@ -36,93 +23,47 @@ create_pixbuf(const gchar * filename)
 int
 main(int argc, char* argv[])
 {
-        if (argc > 1)
+        GtkBuilder *builder;
+        GtkWidget  *window;
+        GtkWidget  *view;
+        GError     *error = NULL;
+
+        /* Init GTK+ */
+        gtk_init( &argc, &argv );
+
+        /* Create new GtkBuilder object */
+        builder = gtk_builder_new();
+        /* Load UI from file. If error occurs, report it and quit application.
+        * Replace "tut.glade" with your saved project. */
+        if( ! gtk_builder_add_from_file( builder, "GUI/GUI.glade", &error ) )
         {
-                int i;
-                time_t start;
-                time_t stop;
-                Sudoku* su;
-                for (i = 1; i < argc; i++)
-                {
-                        printf("%s\n", argv[i]);
-                        su = readSudokuFromFile(argv[i]);
-                        printBoard(su);
-                        time(&start);
-                        solve(su);
-                        time(&stop);
-                        printBoard(su);
-                        free(su);
-                        printf("Solved in %.0fs", difftime(stop, start));
-                        printf("\n");
-                        printf("-------------------------------------------\n");
-                        printf("-------------------------------------------\n");
-                }
+                g_warning( "%s", error->message );
+                g_free( error );
+                return( 1 );
         }
-        else
-        {
-                printf("COMMAND LINE USAGE:\n");
-                printf("./sudoku <SUDOKU_FILE> <SUDOKU_FILE>...\n");
-                
-                GtkWidget* window;
-                GtkWidget* vbox;
-                GtkWidget* halign;
-                
-                GtkWidget* button;
-                GtkWidget* menubar;
-                GtkWidget* fileMenu;
-                GtkWidget* fileMi;
-                GtkWidget* openMi;
-                GtkWidget* sep;
-                GtkWidget* quitMi;
-                
-                gtk_init(&argc, &argv);
-                
-                window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-                gtk_window_set_title(GTK_WINDOW(window), "Sudoku Solver");
-                gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
-                gtk_window_set_default_size(GTK_WINDOW(window), 500, 500);
-                gtk_container_set_border_width(GTK_CONTAINER(window), 15);
-                gtk_window_set_icon(GTK_WINDOW(window), create_pixbuf("img/web.png"));
-                
-                vbox = gtk_vbox_new(FALSE, 0);
-                halign = gtk_alignment_new(0, 0, 0, 0);
-                gtk_container_add(GTK_CONTAINER(window), vbox);
-                
-                menubar = gtk_menu_bar_new();
-                
-                fileMenu = gtk_menu_new();
-                fileMi   = gtk_menu_item_new_with_mnemonic("_File");
-                openMi   = gtk_image_menu_item_new_from_stock(GTK_STOCK_OPEN, NULL);
-                sep      = gtk_separator_menu_item_new();
-                quitMi   = gtk_image_menu_item_new_from_stock(GTK_STOCK_QUIT, NULL);
-                
-                gtk_menu_item_set_submenu(GTK_MENU_ITEM(fileMi), fileMenu);
-                gtk_menu_shell_append(GTK_MENU_SHELL(fileMenu), openMi);
-                gtk_menu_shell_append(GTK_MENU_SHELL(fileMenu), sep);
-                gtk_menu_shell_append(GTK_MENU_SHELL(fileMenu), quitMi);
-                gtk_menu_shell_append(GTK_MENU_SHELL(menubar), fileMi);
-                
-                button = gtk_button_new_with_mnemonic("_Solve");
-                
-                gtk_box_pack_start(GTK_BOX(vbox), menubar, FALSE, FALSE, 0);
-                gtk_container_add(GTK_CONTAINER(halign), button);
-                gtk_container_add(GTK_CONTAINER(vbox), halign);
-                
-                g_signal_connect(button, "clicked",
-                                G_CALLBACK(print_msg), NULL);
-                
-                g_signal_connect(G_OBJECT(quitMi), "activate",
-                                  G_CALLBACK(gtk_main_quit), NULL);
-                
-                g_signal_connect(G_OBJECT(openMi), "activate",
-                                  G_CALLBACK(load_msg), NULL);
-                
-                g_signal_connect_swapped(G_OBJECT(window), "destroy",
-                                G_CALLBACK(gtk_main_quit), NULL);
-                
-                gtk_widget_show_all(window);
-                
-                gtk_main();
-        }
-        return 0;
+
+        /* Get main window pointer from UI */
+        window = GTK_WIDGET( gtk_builder_get_object( builder, "window1" ) );
+        view = GTK_WIDGET( gtk_builder_get_object( builder, "textview" ) );
+        gtk_window_set_icon(GTK_WINDOW(window), create_pixbuf("img/web.png"));
+
+        GtkTextBuffer* buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(view));
+        GtkTextIter end;
+
+        gtk_text_buffer_get_end_iter(buffer, &end);
+        gtk_text_buffer_insert(buffer, &end, "Hello ubuntuforums", -1);
+
+        /* Connect signals */
+        gtk_builder_connect_signals( builder, NULL );
+
+        /* Destroy builder, since we don't need it anymore */
+        g_object_unref( G_OBJECT( builder ) );
+
+        /* Show window. All other widgets are automatically shown by GtkBuilder */
+        gtk_widget_show( window );
+
+        /* Start main loop */
+        gtk_main();
+
+        return( 0 );
 }
